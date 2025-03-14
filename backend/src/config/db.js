71bd4b7d238d3e61a9e324e90aee4b79954daae0
karthik-bha <143622670+karthik-bha@users.noreply.github.com>
2025-03-14 21:00:53 +0530

    Dockerized backend services

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose"
 
 const connectDB = async () => {
   try {
+    // console.log("MongoDB URL:", process.env.MONGO_URL);
     const conn = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB Connected`);
   } catch (error) {
